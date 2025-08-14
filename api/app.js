5a32268f4e0a21fcea132c8cc7627ef987f72331
ZeyzilanKrkmz <zzk603061@gmail.com>
2025-08-14 17:31:12 +0300

    #hatalı isteklerde sist. path'leri ve dosya isimlerini gösteren hatalar res'da veriliyor.Güvenlik açığı.

--- api/app.js
+++ api/app.js
@@ -6,12 +6,13 @@ const createError = require('http-errors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const express = require('express');
 
 
 
 // router mount
 
-const express = require('express');
+/*;
 const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
@@ -43,6 +44,38 @@ app.use(function (err, req, res, next) {
     message: err.message || 'Internal Server Error',
     stack: isDev ? err.stack : undefined
   });
+});*/
+var app=express();
+
+app.set('views',path.join(_dirname,
+    'views'));
+app.set('view engine','ejs');
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(express.static(path.join(_dirname,'public')));
+
+app.use((req,res,next)=>{
+  console.log("Ben app.js'te tanımlanan bir middleware'ım");
+  next();
 });
+
+app.use('/api',require('./routes/index'));
+
+app.use(function (req,res,next){
+  next(createError(404));
+});
+
+app.use(function (err,req,res,next){
+  res.locals.message=err.message;
+  res.locals.error=req.app.get('env')==='development'? err :{};
+
+  res.status(err.status ||500);
+  res.render('error');
+});
+
 
 module.exports = app;
