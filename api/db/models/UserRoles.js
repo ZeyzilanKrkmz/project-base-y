398cfcec92cfb9d4c0e1f95cca376146cb7021f0
ZeyzilanKrkmz <zzk603061@gmail.com>
2025-08-14 16:55:12 +0300

    #bugfix

--- api/db/models/UserRoles.js
+++ api/db/models/UserRoles.js
@@ -1,8 +1,8 @@
 const mongoose=require("mongoose");
 
 const schema=mongoose.Schema({
-    role_id: {type:mongoose.SchemaTypes.ObjectId,required:true},
-    user_id: {type:mongoose.SchemaTypes.ObjectId,required:true}
+    role_id: {type:mongoose.SchemaTypes.ObjectId,required:true,ref:Roles},
+    user_id: {type:mongoose.SchemaTypes.ObjectId,required:true,ref:Users}
 
 },{
     versionKey:false,
